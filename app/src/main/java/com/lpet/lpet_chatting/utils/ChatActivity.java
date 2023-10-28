d64b24e3b4e401bc5834298f755154bd73b3353e
godoy <godoyeli85@gmail.com>
2023-10-28 10:51:51 -0300

    video 12 listo

--- app/src/main/java/com/lpet/lpet_chatting/utils/ChatActivity.java
+++ app/src/main/java/com/lpet/lpet_chatting/utils/ChatActivity.java
@@ -1,4 +1,58 @@
 package com.lpet.lpet_chatting.utils;
 
-public class ChatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.lpet.lpet_chatting.R;
+import com.lpet.lpet_chatting.models.User;
+
+public class ChatActivity extends AppCompatActivity {
+
+    User otherUser;
+    String chatroomId;
+
+    EditText messageInput;
+    ImageButton sendMessageBtn;
+    ImageButton backBtn;
+    TextView otherUsername;
+    RecyclerView recyclerView;
+    ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        //get UserModel
+        otherUser = AndroidUtil.getUserModelFromIntent(getIntent());
+        chatroomId = FirebaseUtil.getChatroomId(FirebaseUtil.currentUserId(), otherUser.getUserId());
+
+        messageInput = findViewById(R.id.chat_message_input);
+        sendMessageBtn = findViewById(R.id.message_send_btn);
+        backBtn = findViewById(R.id.back_btn);
+        otherUsername = findViewById(R.id.other_username);
+        recyclerView = findViewById(R.id.chat_recycler_view);
+        imageView = findViewById(R.id.profile_pic_image_view);
+
+        FirebaseUtil.getOtherProfilePicStorageRef(otherUser.getUserId()).getDownloadUrl()
+                .addOnCompleteListener(t -> {
+                    if (t.isSuccessful()) {
+                        Uri uri = t.getResult();
+                        AndroidUtil.setProfilePic(this, uri, imageView);
+                    }
+                });
+
+        backBtn.setOnClickListener((v) -> {
+            onBackPressed();
+        });
+        otherUsername.setText(otherUser.getUsername());
+    }
 }
